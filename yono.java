a34fabab0a0766128e3bca2661e038ec53dbaa35
kamesh20k <96664751+kamesh20k@users.noreply.github.com>
2024-04-26 12:38:35 +0530

    Update yono.java->remote repo-2

--- yono.java
+++ yono.java
@@ -6,7 +6,7 @@ public class Largest {
 
         if(n1 >= n2) {
             if(n2 >= n3)
-                System.out.println(n1 + " is the largest number.");
+                System.out.println(n2 + " is the largest number.");
             else
                 System.out.println(n3 + " is the largest number.");
         } else {
